77f60283d8230bd8b8d90e0a21014c6304bc1dac
Corwinpro <corwinat@gmail.com>
2021-08-07 12:04:15 +0100

    Release 0.2.0 (#10)
    * REL: bump version to 0.2.0
    
    * GH: fix setup.py and README
    
    * MAINT: bump version

--- setup.py
+++ setup.py
@@ -4,12 +4,18 @@ Setup configuration for installation via pip
 
 from setuptools import setup, find_packages
 
+# read the contents of your README file
+from os import path
+
+this_directory = path.abspath(path.dirname(__file__))
+with open(path.join(this_directory, "README.rst"), encoding="utf-8") as f:
+    long_description = f.read()
 
 # The find_packages function does a lot of the heavy lifting for us w.r.t.
 # discovering any Python packages we ship.
 setup(
     name="pysplines",
-    version="0.1.0.dev2",
+    version="0.3.0.dev0",
     author="Petr Kungurtsev",
     packages=find_packages(),
     # PyPI packages required for the *installation* and usual running of the
@@ -24,4 +30,6 @@ setup(
         "Operating System :: OS Independent",
     ],
     python_requires=">=3.6",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
 )
